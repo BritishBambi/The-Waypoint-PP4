4e600a74fec2a2c9893f3faf78a33fc597270b52
BritishBambi <josefjakubiak@gmail.com>
2022-05-15 02:00:52 +0000

    Create view for game search feature

--- game/views.py
+++ game/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+import requests
 
 
 def home(request):
@@ -6,8 +10,20 @@ def home(request):
 
 
 def search(request):
+    query = request.GET.get('query')
+
+    if query:
+        url = 'https://rawg.io/api/games?key=c4a730f92484414c82505dab317c1720&search=' + query + '&search_exact=1&ordering=-rating&page_size=12'
+        response = requests.get(url)
+        game_data = response.json()
+
+        context = {
+            'query': query,
+            'game_data': game_data,
+        }
+
+        template = loader.get_template('search_results.html')
+
+        return HttpResponse(template.render(context, request))
+
     return render(request, 'search.html')
-
-
-def game_search(request):
-    return render(request, 'search_results.html')
