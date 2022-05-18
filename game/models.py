f6cf042b8d9952ed76860bbdf6c018699d9d4aa6
BritishBambi <josefjakubiak@gmail.com>
2022-05-18 17:54:43 +0000

    Create Review model

--- game/models.py
+++ game/models.py
@@ -15,8 +15,19 @@ class Game(models.Model):
     developer = models.CharField(max_length=100, blank=True)
     genres = models.CharField(max_length=100, blank=True)
     release_date = models.DateField(auto_now_add=False)
-    reviews = models.ManyToManyField(User, related_name='game_reviews', blank=True)
 
     def __str__(self):
         return self.name
 
+
+class Review(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=80)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Review {self.body} by {self.name}"
