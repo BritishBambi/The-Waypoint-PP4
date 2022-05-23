7f33c3dde556c2760baa08dffbd7da5d5c296176
BritishBambi <josefjakubiak@gmail.com>
2022-05-23 15:59:53 +0000

    Create new Game model

--- game/models.py
+++ game/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 
+class Game(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    game_id = models.CharField(max_length=50, blank=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.name
