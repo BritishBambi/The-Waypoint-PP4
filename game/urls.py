c24eb45b8d69d53821adfdaeff63df3ed1478ef3
BritishBambi <josefjakubiak@gmail.com>
2022-05-24 23:52:07 +0000

    Add URL and view to render profile.html

--- game/urls.py
+++ game/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('search/<query>/page/<page_number>', views.pagination, name='pagination'),
     path('game/<game_id>', views.game_details, name='game_details'),
+    path('profile/', views.profile, name='profile'),
 ]
