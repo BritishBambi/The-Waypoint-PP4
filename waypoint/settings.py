31dc907721af904175a3946b039f9e8185cf85d1
BritishBambi <josefjakubiak@gmail.com>
2022-05-25 20:37:03 +0000

    Add Email SMTP test

--- waypoint/settings.py
+++ waypoint/settings.py
@@ -149,3 +149,10 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USERNAME')
+EMAIL_HOST_USER = os.environ.get('EMAIL_PASSWORD')
