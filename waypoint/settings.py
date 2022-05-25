21b8ee7b2a9c006207b99bc12cb05b2ddae86c65
BritishBambi <josefjakubiak@gmail.com>
2022-05-25 21:29:21 +0000

    Switch to sendgrid based on issues with google smtp

--- waypoint/settings.py
+++ waypoint/settings.py
@@ -151,9 +151,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USERNAME')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_API_KEY')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
