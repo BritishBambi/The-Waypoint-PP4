2000c118fb6823b7feca3fbe40d58dd6858edb84
BritishBambi <josefjakubiak@gmail.com>
2022-05-25 01:40:00 +0000

    Create Profile Model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+from game.models import Game
 
 # Create your models here.
+
+default_img = 'https://res.cloudinary.com/jojocloudci/image/upload/v1653441917/default-profile_fb2lrf.jpg'
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name='profile')
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    bio = models.CharField(max_length=700, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    profile_image = CloudinaryField('image', default=default_img)
+    to_play = models.ManyToManyField(Game, related_name='to_play')
+    played = models.ManyToManyField(Game, related_name='played')
+
+    def __str__(self):
+        return str(self.user)
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def profile(request):
-    return render(request, 'profile.html')#
+    return render(request, 'profile.html')
