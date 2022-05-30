1d3229c4b408f15d2baa4f20f0656d8f16357e62
BritishBambi <josefjakubiak@gmail.com>
2022-05-30 18:51:43 +0000

    Register Review model into admin panel

--- game/admin.py
+++ game/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Game
+from .models import Game, Review
 
 # Register your models here.
 
 admin.site.register(Game)
+admin.site.register(Review)
--- game/models.py
+++ game/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Game(models.Model):
@@ -7,3 +8,28 @@ class Game(models.Model):
 
     def __str__(self):
         return self.name
+
+
+RATE_CHOICES = [
+    (1, '1 - Trash'),
+    (2, '2 - Terrible'),
+    (3, '3 - Bad'),
+    (4, '4 - Not Great'),
+    (5, '5 - OK'),
+    (6, '6 - Decent'),
+    (7, '7 - Good'),
+    (8, '8 - Very Good'),
+    (9, '9 - Almost Perfect'),
+    (10, '10 - Masterpiece'),
+]
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(max_length=3000, blank=True)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+
+    def __str__(self):
+        return self.game.name
