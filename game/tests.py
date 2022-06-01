512be034e309c9a1491b1a77cb48263dcbd8bb45
BritishBambi <josefjakubiak@gmail.com>
2022-06-01 22:20:03 +0000

    Create TestViews for testing game views, set up test user

--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+    """
+    Testing for game views
+    """
+    def setUp(self):
+        """ Create test login user"""
+        username = "jojo"
+        pswd = "Trials"
+        user_model = get_user_model()
+        self.user = user_model.objects.create_user(username=username,
+                                                   password=pswd,
+                                                   is_superuser=True)
+        logged_in = self.client.login(username=username, password=pswd)
+
+        self.assertTrue(logged_in)
